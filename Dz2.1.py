94c3f3f8ced2e53ba3647517034ec6daa408ec4b
K_Khalkov <halcov12@gmail.com>
2024-11-07 21:49:36 +0200

    Dz2.1

---
+++ Dz2.1.py
@@ -0,0 +1,9 @@
+num = int(input('Введите 4х значное числo:'))
+num1 = num // 1000
+num2 = (num // 100) % 10
+num3 = (num // 10) % 10
+num4 = (num // 1) % 10
+print(num1)
+print(num2)
+print(num3)
+print(num4)
