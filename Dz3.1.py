a0f093d0afff9dec77731ba654b82fa0f5e60e2e
K_Khalkov <halcov12@gmail.com>
2024-11-11 21:35:39 +0200

    Dz3.1

---
+++ Dz3.1.py
@@ -0,0 +1,14 @@
+num1 = float(input('Введите первое число:'))
+num2 = float(input('Введите второе число:'))
+act = input('Введите действие:')
+if act == '+':
+    print('Результат', num1 + num2)
+elif act == '-':
+    print('Результат', num1 - num2)
+elif act == '*':
+    print('Результат', num1 * num2)
+elif act == '/':
+    if num2 == 0:
+        print("На ноль делить нельзя")
+    else:
+        print('Результат', num1 / num2)
